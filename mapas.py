59eaceab0b70886c239ef0baf1bf42ea11653168
Herald Fortunato <heraldfortunato@outlook.es>
2022-11-06 00:19:41 -0300

    Mapas com python

--- mapas.py
+++ mapas.py
@@ -9,7 +9,7 @@ mapa = folium.Map(
     zoom_start=16
 )
 
-#  Adiciona o marcador no mapa
+#  Adiciona o marcador no mapa  
 folium.Marker(
     [-23.550093493433984, -46.6341472592547],
     popup='<i>Praça da Sé</i>',
